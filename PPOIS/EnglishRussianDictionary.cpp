32e116e73ea66b4b6b8b9afad6abef095df26757
Elize3000 <115619115+Elize3000@users.noreply.github.com>
2023-12-12 20:25:23 +0300

    Update EnglishRussianDictionary.cpp

--- PPOIS/EnglishRussianDictionary.cpp
+++ PPOIS/EnglishRussianDictionary.cpp
@@ -4,234 +4,152 @@
 */
 
 
-#include "pch.h"
-#include <fstream>
 #include "EnglishRussianDictionary.h"
 #include <string>
 
 
-	EnglishRussianDictionary::EnglishRussianDictionary()
-	{
-		english_russian_dictionary_tree_root_ = NULL;
-	}
-	/**
+EnglishRussianDictionary::EnglishRussianDictionary()
+{
+	english_russian_dictionary_tree_root_ = NULL;
+}
+
+/**
 	*Clears the memory
-	*/
-	void EnglishRussianDictionary::DeleteAll(PairOfWords* root)
-	{
-		if (root == NULL)
-			return;
-		DeleteAll(root->right_child_);
-		DeleteAll(root->right_child_);
-		delete root;
-	}
+*/
 
-	EnglishRussianDictionary::~EnglishRussianDictionary()
-	{
-		DeleteAll(english_russian_dictionary_tree_root_);
-		english_russian_dictionary_tree_root_ == NULL;
-	}
+EnglishRussianDictionary::~EnglishRussianDictionary()
+{
+	DeleteAll(english_russian_dictionary_tree_root_);
+}
 
-	/**
-	*Adds the pair of words. Also checs if the added element already in tree
-	*/
-	bool EnglishRussianDictionary::AddNewPairOfWords(std::string english_word, std::string russian_translation)
-	{
-		PairOfWords* new_pair_of_words = new PairOfWords;
-		new_pair_of_words->english_word_ = english_word;
-		new_pair_of_words->russian_tranlation_ = russian_translation;
-		PairOfWords* root = english_russian_dictionary_tree_root_;
-	    if(root == NULL)
-			root = new_pair_of_words;
-		return root;
-		root = FindPerentForNew(root, new_pair_of_words);
-		if (root != NULL && root->english_word_ > new_pair_of_words->english_word_)
-			root->left_child_ = new_pair_of_words;
-		else if (root != NULL)
-		{
-			root->right_child_ = new_pair_of_words;
-		}
-		else return false;
-		return true;
-	}
+void EnglishRussianDictionary::DeleteAll(PairOfWords* root)
+{
+	if (root == NULL)
+		return;
+	DeleteAll(root->left_child_);
+	DeleteAll(root->right_child_);
+	delete root;
+}
 
-	/**
-	*Searches for plsce, were element coud be instolen
-	*/
-
-	PairOfWords* EnglishRussianDictionary::FindPerentForNew(PairOfWords* root, PairOfWords* new_element)
-	{
-		if (root->english_word_ == new_element->english_word_)
-			return NULL;
-		if (root->english_word_ > new_element->english_word_ && root->right_child_ != NULL)
-			FindPerentForNew(root->right_child_, new_element);
-		else if (root->english_word_ < new_element->english_word_ && root->left_child_ != NULL)
-			FindPerentForNew(root->left_child_, new_element);
-		return root;
-
-	}
-	/**
+/**
 	*Searches for an element
-	*/
+*/
 
-	PairOfWords* EnglishRussianDictionary::FindElement(std::string english_word)
-	{
-		PairOfWords* current_search_point_ = english_russian_dictionary_tree_root_;
-		if (current_search_point_ == NULL)
-			return NULL;
-			while (true)
-			{
-				if (current_search_point_->english_word_ > english_word)
-				{
-					if (current_search_point_->right_child_ != NULL)
-						current_search_point_ = current_search_point_->right_child_;
-					else return NULL;
-				}if (current_search_point_->english_word_ < english_word)
-				{      
-					 if (current_search_point_->left_child_ != NULL)
-						current_search_point_ = current_search_point_->left_child_;
-					else return NULL;
-				}
-			}if (current_search_point_->english_word_ == english_word)
-				return current_search_point_;
-	}
-
-	/**
-	*searches for elemen, thet is perent 
-	*/
-
-	PairOfWords* EnglishRussianDictionary::FindPerantElement(std::string english_word, PairOfWords* root)
-	{
-		if (root->left_child_->english_word_ == english_word || root->right_child_->english_word_ == english_word)
-			return root;
-		if (root->english_word_ > english_word)
-			return FindPerantElement(english_word, root->right_child_);
-		else if (root->english_word_ < english_word)
-			return FindPerantElement(english_word, root->right_child_);
-		
-	}
-
-	/**
-	*Searches and deletes element
-	*/
-
-	bool EnglishRussianDictionary::DeleteEnglishWord(std::string english_word)
-	{
-		if (english_russian_dictionary_tree_root_ == NULL)
-			return false;
-		
-		PairOfWords* faunded_element= FindElement(english_word);
-		if (faunded_element == NULL)
-			return false;
-		if (faunded_element->left_child_ == NULL && faunded_element->right_child_ == NULL)
-		{
-			return DeleteEnglishWordOneChild(faunded_element);
-		}else if (faunded_element->left_child_ == NULL || faunded_element->right_child_ == NULL)
-		{
-			return DeleteEnglishWordNoChildren(faunded_element);
-		}else
-		{
-			DeleteEnglishWordTwoChildren(faunded_element);
-		}
-
-
-	}
-
-	/**
-	*Different algorithm for different situations
-	*/
-
-	bool EnglishRussianDictionary::DeleteEnglishWordNoChildren(PairOfWords* root)
-	{
-		PairOfWords* Perent = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
-		if (Perent->left_child_ == root)
-			Perent->left_child_ = NULL;
-		if (Perent->right_child_ == root)
-			Perent->right_child_ = NULL;
-		delete root;
+bool EnglishRussianDictionary::IsContain(std::string english_word)
+{
+	if (FindElement(english_word, english_russian_dictionary_tree_root_) != NULL)
 		return true;
+	return false;
+}
 
-	}
-	bool EnglishRussianDictionary::DeleteEnglishWordOneChild(PairOfWords* root)
+std::string EnglishRussianDictionary::operator[](std::string english_word)
+{
+	PairOfWords* faunded_element = FindElement(english_word, english_russian_dictionary_tree_root_);
+	if (faunded_element == NULL)
+		return "";
+	return faunded_element->russian_tranlation_;
+}
+
+PairOfWords* EnglishRussianDictionary::FindElement(std::string english_word, PairOfWords* root)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->english_word_ > english_word)
 	{
-		PairOfWords* Perent = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
-		if (Perent->left_child_ == root)
-		{
-			if (root->left_child_ == NULL)
-			{
-				Perent->left_child_ = root->right_child_;
-			}
-			else
-				Perent->right_child_ = root->left_child_;
-		}else if (root->left_child_ == NULL)
-		{
-			Perent->left_child_ = root->right_child_;
-		}
-		else
-			Perent->right_child_ = root->left_child_;
-		delete root;
+		if (root->right_child_ != NULL)
+			return FindElement(english_word, root->right_child_);
+		return NULL;
+	}
+	if (root->english_word_ < english_word)
+	{
+		if (root->left_child_ != NULL)
+			return FindElement(english_word, root->left_child_);
+		return NULL;
+	}
+	return root;
+}
+
+/**
+*Searches for plsce, were element coud be instolen
+*/
+
+PairOfWords* EnglishRussianDictionary::FindPerentForNew(std::string english_word, PairOfWords* root)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->english_word_ > english_word)
+	{
+		if (root->right_child_ != NULL)
+			return FindPerentForNew(english_word, root->right_child_);
+		return root;
+	}
+	if (root->english_word_ < english_word)
+	{
+		if (root->left_child_ != NULL)
+			return FindPerentForNew(english_word, root->left_child_);
+		return root;
+	}
+	return NULL;
+}
+
+/**
+	*Adds the pair of words. Also checs if the added element already in tree
+*/
+
+bool EnglishRussianDictionary::AddNewPairOfWords(std::string english_word, std::string russian_translation)
+{
+	PairOfWords* new_element = new PairOfWords;
+	new_element->english_word_ = english_word;
+	new_element->russian_tranlation_ = russian_translation;
+	if (english_russian_dictionary_tree_root_ == NULL)
+	{
+		english_russian_dictionary_tree_root_ = new_element;
 		return true;
 	}
-	bool EnglishRussianDictionary::DeleteEnglishWordTwoChildren(PairOfWords* root)
-	{
-		PairOfWords* Perent = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
-		PairOfWords* Child = root->right_child_;
-		while (Child->left_child_!= NULL)
-		{
-			Child = Child->left_child_;
-		}
-		if (Perent->right_child_ == root)
-		{
-			Perent->right_child_ == Child;
-		}
-		else
-			Perent->left_child_ == Child;
-		return true;
-
-
-	}
-
-	/**
-	*Changes translation for english word
-	*/
-
-	bool EnglishRussianDictionary::ChangeTranslation(std::string english_word, std::string russian_translation)
-	{
-		PairOfWords* current_search_point_ = FindElement(english_word);
-		if (current_search_point_ != NULL)
-		{
-			current_search_point_->russian_tranlation_ = russian_translation;
-			return true;
-		}
+	PairOfWords* root = FindPerentForNew(english_word, english_russian_dictionary_tree_root_);
+	if (root == NULL)
 		return false;
-	}
-
-	/**
-	*Opperation override
-	*/
-
-	std::string EnglishRussianDictionary::operator [](std::string english_word)
+	if (root->english_word_ > english_word)
 	{
-		PairOfWords* faunded_elemeent = FindElement(english_word);
-		if (faunded_elemeent == NULL)
-			return "";
-		else return faunded_elemeent->russian_tranlation_;
+		root->right_child_ = new_element;
+		return true;
 	}
+	root->left_child_ = new_element;
+	return true;
+}
 
-	/**
-	*ACounts the namber of words in the dictionary
-	*/
+/**
+*Counts namber of words in dictinary
+*/
 
-	int EnglishRussianDictionary::CountNamberOfWords(PairOfWords* root)
-	{
-		if (root == NULL)
-			return 0;
-		return CountNamberOfWords(root->left_child_) + CountNamberOfWords(root->right_child_) + 1;
-	}
+int EnglishRussianDictionary::CountNamberOfWords()
+{
+	return CountNamberOfWordsLogic(english_russian_dictionary_tree_root_);
+}
 
-	/**
-	*Enters dictionary from file
-	*/
+int EnglishRussianDictionary::CountNamberOfWordsLogic(PairOfWords* root)
+{
+	if (root == NULL)
+		return 0;
+	return CountNamberOfWordsLogic(root->left_child_) + CountNamberOfWordsLogic(root->right_child_) + 1;
+}
+
+/**
+*Changes the translation
+*/
+
+bool EnglishRussianDictionary::ChangeTranslation(std::string english_word, std::string russian_translation)
+{
+	PairOfWords* current_position = FindElement(english_word, english_russian_dictionary_tree_root_);
+	if (current_position == NULL)
+		return false;
+	current_position->russian_tranlation_ = russian_translation;
+	return true;
+}
+
+/**
+*Reads information from file
+*/
 
 bool EnglishRussianDictionary::ReadDictionaryFromFile(EnglishRussianDictionary& new_dictionary, std::string filename)
 {
@@ -244,7 +162,7 @@ bool EnglishRussianDictionary::ReadDictionaryFromFile(EnglishRussianDictionary& 
 		{
 			for (int i = 0; i <= words.size(); i++)
 			{
-				if (flag == 2 && english != "" && russian != "" && words[i] == ' ')
+				if (flag == 2 && english != "" && russian != "" && (words[i] == ' ' || words[i] == '\n' || i == words.size()))
 				{
 					AddNewPairOfWords(english, russian);
 					english = "";
@@ -254,8 +172,9 @@ bool EnglishRussianDictionary::ReadDictionaryFromFile(EnglishRussianDictionary& 
 				if ((flag == 1 && english != "" && words[i] == ' '))
 				{
 					flag = 2;
+					continue;
 				}
-				if (words[i] >= 'a' && words[i] <= 'Z' || words[i] >= '‡' && words[i] <= 'ﬂ')
+				if (words[i] >= 'A' && words[i] <= 'z' || words[i] >= '–ê' && words[i] <= '—è')
 				{
 					if (flag == 1)
 						english += words[i];
@@ -267,12 +186,151 @@ bool EnglishRussianDictionary::ReadDictionaryFromFile(EnglishRussianDictionary& 
 		}
 		return true;
 	}
-
 }
 
-bool EnglishRussianDictionary::IsContain(std::string english_word)
+/**
+	*algorithm, that deletes element without children
+*/
+
+bool EnglishRussianDictionary::DeleteEnglishWordNoChildren(PairOfWords* root)
 {
-	if (FindElement(english_word) != NULL)
+	PairOfWords* parent_of_element = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
+	if (parent_of_element == NULL)
+	{
+		english_russian_dictionary_tree_root_ = NULL;
+	}else
+	if (parent_of_element->left_child_ == root)
+	{
+		parent_of_element->left_child_ = NULL;
+	}
+	else
+		parent_of_element->right_child_ = NULL;
+	delete root;
+	return true;
+}
+
+/**
+	*algorithm, that deletes element with one child
+*/
+
+bool EnglishRussianDictionary::DeleteEnglishWordOneChild(PairOfWords* root)
+{
+	PairOfWords* parent_of_element = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
+	if (parent_of_element == NULL)
+	{
+		if (root->left_child_ != NULL)
+			english_russian_dictionary_tree_root_ = root->left_child_;
+		else
+		{
+			english_russian_dictionary_tree_root_ = root->right_child_;
+		}
+	}
+	else
+	if (parent_of_element->left_child_ == root)
+	{
+		if (root->left_child_ != NULL)
+			parent_of_element->left_child_ = root->left_child_;
+		else
+		{
+			parent_of_element->left_child_ = root->right_child_;
+		}
+	}
+	else
+	{
+		if (root->left_child_ != NULL)
+			parent_of_element->right_child_ = root->left_child_;
+		else
+		{
+			parent_of_element->right_child_ = root->right_child_;
+		}
+	}
+	delete root;
+	return true;
+}
+
+/**
+	*algorithm, that deletes element with two children
+*/
+
+bool EnglishRussianDictionary::DeleteEnglishWordTwoChildren(PairOfWords* root)
+{
+	PairOfWords* parent_of_element = FindPerantElement(root->english_word_, english_russian_dictionary_tree_root_);
+	PairOfWords* element_for_swap = root->left_child_;
+	if (element_for_swap->right_child_ == NULL)
+	{
+		element_for_swap->right_child_ = root->right_child_;
+		if (parent_of_element == NULL)
+		{
+			english_russian_dictionary_tree_root_ = element_for_swap;
+		}
+		else
+			if (parent_of_element->left_child_ == root)
+			{
+				parent_of_element->left_child_ = element_for_swap;
+			}
+			else
+				parent_of_element->right_child_ = element_for_swap;
+		delete root;
 		return true;
-	return false;
+	}
+	while (element_for_swap->right_child_ != NULL)
+	{
+		element_for_swap = element_for_swap->right_child_;
+	}
+	PairOfWords* buf_to_change_adress = FindPerantElement(element_for_swap->english_word_, english_russian_dictionary_tree_root_);
+	buf_to_change_adress->right_child_ = NULL;
+
+	element_for_swap->right_child_ = root->right_child_;
+	element_for_swap->left_child_ = root->left_child_;
+	if (parent_of_element == NULL)
+	{
+		english_russian_dictionary_tree_root_ = element_for_swap;
+	} else
+	if (parent_of_element->left_child_ == root)
+	{
+		parent_of_element->left_child_ = element_for_swap;
+	}
+	else
+		parent_of_element->right_child_ = element_for_swap;
+	delete root;
+	return true;
+}
+
+/**
+	*searches for elemen, that is perent
+*/
+
+PairOfWords* EnglishRussianDictionary::FindPerantElement(std::string english_word, PairOfWords* root)
+{
+	if (root->english_word_ == english_word)
+		return NULL;
+	if (root->right_child_!= NULL && root->right_child_->english_word_ == english_word)
+		return root;
+	if (root->left_child_!= NULL && root->left_child_->english_word_ == english_word)
+		return root;
+	if (root->english_word_ > english_word)
+		return FindPerantElement(english_word, root->right_child_);
+	return FindPerantElement(english_word, root->left_child_);
+}
+
+/**
+	*Searches and deletes element
+*/
+
+bool EnglishRussianDictionary::DeleteEnglishWord(std::string english_word)
+{
+	PairOfWords* element = FindElement(english_word, english_russian_dictionary_tree_root_);
+	if (element == NULL)
+		return false;
+	if (element->left_child_ == NULL && element->right_child_ == NULL)
+	{
+		DeleteEnglishWordNoChildren(element);
+	}
+	else
+	{
+		if (element->left_child_ == NULL || element->right_child_ == NULL)
+			DeleteEnglishWordOneChild(element);
+		else DeleteEnglishWordTwoChildren(element);
+	}
+	return true;
 }
